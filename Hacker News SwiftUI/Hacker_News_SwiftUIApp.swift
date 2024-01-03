38a9dd078f522ddcacf28637e5f0919f0ffeda2b
Shreyash <shreyashp47@gmail.com>
2024-01-03 07:29:10 +0530

    Initial Commit

---
+++ Hacker News SwiftUI/Hacker_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUIApp.swift
+//  Hacker News SwiftUI
+//
+//  Created by Shreyash Pattewar on 03/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
